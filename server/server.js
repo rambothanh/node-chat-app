a413ba17d60abc3be0e6597e0db76f92fd23e79e
unknown <rambothanh1985@gmail.com>
2018-06-12 22:13:59 +0700

    emit all connected

--- server/server.js
+++ server/server.js
@@ -17,25 +17,25 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 //Mọi event của client cần được xử lý để bên trong 'connection'
- io.on ('connection', (socket) => {
+ io.on('connection', (socket) => {
  	console.log('New user connected');
 
  	//Lắng nghe sự kiện của cleint, đối số thứ 2 là đối tượng
  	//để làm đối số của emit máy khách, là đối số thứ nhất của callback 
  	//
- 	socket.emit('newMessage',{
- 		from: 'thanh',
- 		text: 'nguyeenx trong thanh',
- 		createAt: 314234
- 	});
-
+ 	
      socket.on('createMessage', (message) => {
      	console.log('createMessage', message);
+     	io.emit('newMessage', {
+     		from: message.from,
+     		text: message.text,
+     		createAt: new Date().getTime()
+     	});
      });
 
  	socket.on('disconnect', () => {
  		console.log('User was disconnected');
- 	});
+ 	}); 
  });
 
 
