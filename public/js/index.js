a06e4bb1545addf9d956db164e1dc289f8eb8a48
unknown <rambothanh1985@gmail.com>
2018-06-12 22:03:24 +0700

    test socket

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();
+//Tạo sự kiện ở máy khách
+socket.on('connect', function() {
+    console.log('connected to server');
+
+    socket.emit('createMessage',{
+    	from: 'TrongThanh',
+    	text: 'Nguyen Trong Thanh'
+    });
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+	console.log('newMessage', message);
+});
+
+
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname , '../public')
 
@@ -7,14 +9,36 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
+//Tạo máy chủ bằng cách sử dụng http
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.join(publicPath, 'index.html'))
-// });
+//Mọi event của client cần được xử lý để bên trong 'connection'
+ io.on ('connection', (socket) => {
+ 	console.log('New user connected');
+
+ 	//Lắng nghe sự kiện của cleint, đối số thứ 2 là đối tượng
+ 	//để làm đối số của emit máy khách, là đối số thứ nhất của callback 
+ 	//
+ 	socket.emit('newMessage',{
+ 		from: 'thanh',
+ 		text: 'nguyeenx trong thanh',
+ 		createAt: 314234
+ 	});
+
+     socket.on('createMessage', (message) => {
+     	console.log('createMessage', message);
+     });
+
+ 	socket.on('disconnect', () => {
+ 		console.log('User was disconnected');
+ 	});
+ });
 
 
-
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`App starting at port ${port}`);
 });
