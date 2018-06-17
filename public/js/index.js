353e4d6cb74dd937d65481937221a383ac0d44fb
unknown <rambothanh1985@gmail.com>
2018-06-17 22:49:05 +0700

    them mustache va dieu chinh code thanh mustache

--- public/js/index.js
+++ public/js/index.js
@@ -15,29 +15,34 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-	console.log('newMessage', message);
-
 	var formattedTime = moment(message.createAt).format('h:mm a');
-
-
-	//jQuery('html') : tạo phần tử html
-	//jQuery('<li></li>') : tạo phần tử <li></li>
-	var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    // .append(): Thành phần được chèn thêm nội dung, 
-    // nội dung này thường được sắp xếp ở vị trí sau cùng.
-    jQuery('#messages').append(li);
+	var template = jQuery('#message-template').html();
+	var html = Mustache.render(template, {
+		text: message.text,
+		from: message.from,
+		createAt: formattedTime
+	});
+	jQuery('#messages').append(html);
+	
 });
 
 
 socket.on('newLocationMessage', function(message) {
-	var li = jQuery('<li></li>');
-	var a = jQuery('<a target="_blank">My current location</a>');
 	var formattedTime = moment(message.createAt).format('h:mm a');
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href',message.url);
-	li.append(a);
-	jQuery('#messages').append(li);
+	var template = jQuery('#location-message-template').html();
+	var html = Mustache.render(template, {
+		url: message.url,
+		from: message.from,
+		createAt: formattedTime
+	});
+	jQuery('#messages').append(html);
+	// var li = jQuery('<li></li>');
+	// var a = jQuery('<a target="_blank">My current location</a>');
+	// var formattedTime = moment(message.createAt).format('h:mm a');
+	// li.text(`${message.from} ${formattedTime}: `);
+	// a.attr('href',message.url);
+	// li.append(a);
+	// jQuery('#messages').append(li);
 });
 
 
